171fc8bf2e885eec98a08f4dd7ddda25ebb1b4a3
NencyShah <nency_shah_knoxpo@gmail.com>
2018-05-24 16:57:02 +0530

    second commit

--- app/src/main/java/com/example/tejas/launcher/LauncherFragment.java
+++ app/src/main/java/com/example/tejas/launcher/LauncherFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,8 +14,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LauncherFragment extends Fragment {
@@ -34,23 +38,23 @@ public class LauncherFragment extends Fragment {
 
         mRecyclerView = v.findViewById(R.id.launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-setupAdapter();
+        setupAdapter();
         return v;
     }
 
     private void setupAdapter() {
         Intent startupIntent = new Intent(Intent.ACTION_MAIN);
-       startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
         PackageManager pm = getActivity().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
-        /*Collections.sort(activities, new Comparator<ResolveInfo>() {
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
             public int compare(ResolveInfo a, ResolveInfo b) {
                 PackageManager pm = getActivity().getPackageManager();
                 return String.CASE_INSENSITIVE_ORDER.compare(
                         a.loadLabel(pm).toString(),
                         b.loadLabel(pm).toString());
             }
-        });*/
+        });
         mRecyclerView.setAdapter(new ActivityAdapter(activities));
 
     }
@@ -58,10 +62,12 @@ setupAdapter();
     private class ActivityHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ResolveInfo mResolveInfo;
         private TextView mNameTextView;
+        private ImageView mAppIV;
 
         public ActivityHolder(View itemView) {
             super(itemView);
-            mNameTextView = (TextView) itemView;
+            mNameTextView = itemView.findViewById(R.id.txt_name);
+            mAppIV = itemView.findViewById(R.id.iv_app);
             mNameTextView.setOnClickListener(this);
         }
 
@@ -69,6 +75,9 @@ setupAdapter();
             mResolveInfo = resolveInfo;
             PackageManager pm = getActivity().getPackageManager();
             String appName = mResolveInfo.loadLabel(pm).toString();
+            Drawable Image = mResolveInfo.loadIcon(pm);
+
+            mAppIV.setImageDrawable(Image);
             mNameTextView.setText(appName);
         }
 
@@ -77,28 +86,33 @@ setupAdapter();
             ActivityInfo activityInfo = mResolveInfo.activityInfo;
             Intent i = new Intent(Intent.ACTION_MAIN)
                     .setClassName(activityInfo.applicationInfo.packageName,
-                            activityInfo.name).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);;
+                            activityInfo.name);
+            ;
             startActivity(i);
         }
     }
 
     private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder> {
         private final List<ResolveInfo> mActivities;
+
         public ActivityAdapter(List<ResolveInfo> activities) {
             mActivities = activities;
         }
+
         @Override
         public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View view = layoutInflater
-                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+                    .inflate(R.layout.list_apps, parent, false);
             return new ActivityHolder(view);
+
         }
+
         @Override
         public void onBindViewHolder(ActivityHolder activityHolder, int position) {
             ResolveInfo resolveInfo = mActivities.get(position);
             activityHolder.bindActivity(resolveInfo);
-            }
+        }
 
         @Override
         public int getItemCount() {
